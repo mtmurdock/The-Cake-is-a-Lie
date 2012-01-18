b15ae9672421abdd59c7caff9fdb7b501528db6a
Matthew Murdock <mtmurdock@bender.local>
2012-01-17 22:44:19 -0700

    The Cake is a Lie! App is in most basic form suitable for submission for class achievement. Loads random quote from plist, chooses a random y vector, and scrolls the quote from left to right (repeat).

--- TheCakeIsALie/MMAppDelegate.h
+++ TheCakeIsALie/MMAppDelegate.h
@@ -9,7 +9,15 @@
 #import <UIKit/UIKit.h>
 
 @interface MMAppDelegate : UIResponder <UIApplicationDelegate>
+{
+    UILabel* _label;
+    NSArray* _strings;
+    NSTimer* _timer;
+}
 
+@property (nonatomic, retain) UILabel* label;
 @property (strong, nonatomic) UIWindow *window;
+
+- (void) backgroundTask:(id)sender;
 
 @end
--- TheCakeIsALie/MMAppDelegate.m
+++ TheCakeIsALie/MMAppDelegate.m
@@ -10,13 +10,7 @@
 
 @implementation MMAppDelegate
 
-@synthesize window = _window;
-
-- (void)dealloc
-{
-    [_window release];
-    [super dealloc];
-}
+@synthesize window = _window, label = _label;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
@@ -24,46 +18,58 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    // read in plist of quotes.
+    NSString* path = [[NSBundle mainBundle] pathForResource:@"quotes" ofType:@"plist"];
+    NSDictionary* dict = [[NSDictionary alloc] initWithContentsOfFile:path];
+    _strings = (NSArray*)[dict objectForKey:@"Root"];
+    
+    // init label and add to window. 
+    self.label = [[UILabel alloc] init];
+    [self.label setFont:[UIFont systemFontOfSize:30]];
+    [self.window addSubview:self.label];
+    
+    // set up repeating timer
+    _timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(backgroundTask:) userInfo:nil repeats:true];
+    
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
+- (void) backgroundTask:(id) sender
 {
-    /*
-     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-     */
+    // if is not visible, start the next animation.
+    if (!CGRectIntersectsRect(self.window.bounds, self.label.frame)) {
+        int index = arc4random() % _strings.count;
+        [self.label setText:[_strings objectAtIndex:index]];
+        // determine size of view
+        CGSize textSize = [[self.label text] sizeWithFont:[self.label font]];
+        // choose a random y value.
+        int y = arc4random() % (int)(self.window.bounds.size.height - textSize.height);
+        CGRect rect = CGRectMake(self.window.bounds.size.width, y, textSize.width, textSize.height);
+        [self.label setFrame:rect];
+    }
+    
+    //  move label to the left
+    CGRect newRect = self.label.frame;
+    newRect.origin.x -= 150;
+    
+    // animate the transformation
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationDuration:1];
+    [UIView setAnimationCurve:UIViewAnimationCurveLinear];
+    
+    [self.label setFrame:newRect];
+    
+    [UIView commitAnimations];
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
+- (void)dealloc
 {
-    /*
-     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-     If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-     */
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    /*
-     Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-     */
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    /*
-     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-     */
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    /*
-     Called when the application is about to terminate.
-     Save data if appropriate.
-     See also applicationDidEnterBackground:.
-     */
+    [_window release];
+    [_label release];
+    [_strings release];
+    [_timer release];
+    [super dealloc];
 }
 
 @end
